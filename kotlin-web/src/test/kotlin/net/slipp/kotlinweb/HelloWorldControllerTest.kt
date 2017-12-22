a5d3907fc0001ded686b5a4d0064bd58181c7cc2
javajigi <javajigi@gmail.com>
2017-12-22 17:55:26 +0900

    add Hello World controller, test

---
+++ kotlin-web/src/test/kotlin/net/slipp/kotlinweb/HelloWorldControllerTest.kt
@@ -0,0 +1,29 @@
+package net.slipp.kotlinweb
+
+import org.assertj.core.api.Assertions
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.reactive.server.WebTestClient
+
+@RunWith(SpringRunner::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HelloWorldControllerTest {
+    @Autowired
+    lateinit var client : WebTestClient
+
+    @Test
+    fun helloworld() {
+        client.get().uri("/helloworld")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk
+                .expectBody()
+                .consumeWith {
+                    Assertions.assertThat(String(it.responseBody)).isEqualTo("Hello World")
+                }
+    }
+}
