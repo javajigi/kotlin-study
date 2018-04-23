6b37c564ef5cfea44f78753a56ccc196d1bdc683
javajigi <javajigi@gmail.com>
2018-04-23 23:19:52 +0900

    feat(lotto): calculate profit

--- kotlin-lotto/src/main/kotlin/lotto/domain/MatchingResult.kt
+++ kotlin-lotto/src/main/kotlin/lotto/domain/MatchingResult.kt
@@ -1,12 +1,14 @@
 package lotto.domain
 
+infix fun Int.fdiv(i: Int): Double = this / i.toDouble();
+
 class MatchingResult {
-    private val result = listOf(
-            MatchingCountPerLotto(Rank.FIRST),
-            MatchingCountPerLotto(Rank.SECOND),
-            MatchingCountPerLotto(Rank.THIRD),
-            MatchingCountPerLotto(Rank.FORTH),
-            MatchingCountPerLotto(Rank.FIFTH))
+    private val result = mapOf(
+            Rank.FIRST to MatchingCountPerLotto(Rank.FIRST),
+            Rank.SECOND to MatchingCountPerLotto(Rank.SECOND),
+            Rank.THIRD to MatchingCountPerLotto(Rank.THIRD),
+            Rank.FORTH to MatchingCountPerLotto(Rank.FORTH),
+            Rank.FIFTH to MatchingCountPerLotto(Rank.FIFTH))
 
     fun matchCountByRank(rank: Rank): Int {
         return findMatchingCount(rank)?.matchingCount ?: 0
@@ -17,18 +19,27 @@ class MatchingResult {
     }
 
     fun findMatchingCount(rank: Rank): MatchingCountPerLotto? {
-        return result.filter { it.isMatch(rank) }.firstOrNull()
+        return result.get(rank)
+    }
+
+    fun totalWinningMoney(): Int {
+        return result.values.map { it.winningMoney() }.sum();
+    }
+
+    fun getProfit(investMoney: Int): Double {
+        val profit = totalWinningMoney().minus(investMoney);
+        return profit.fdiv(investMoney) * 100
     }
 
     class MatchingCountPerLotto(
             private val rank: Rank, var matchingCount: Int = 0) {
-        fun isMatch(rank: Rank): Boolean {
-            return this.rank == rank;
-        }
-
         fun match(): Int {
             matchingCount++
             return matchingCount
+        }
+
+        fun winningMoney(): Int {
+            return rank.winningMoney(matchingCount)
         }
     }
 }
--- kotlin-lotto/src/test/kotlin/lotto/domain/MatchingResultTest.kt
+++ kotlin-lotto/src/test/kotlin/lotto/domain/MatchingResultTest.kt
@@ -20,4 +20,20 @@ object MatchingResultTest : Spek ({
             assertThat(matchingResult.matchCountByRank(Rank.NONE)).isEqualTo(0)
         }
     }
+
+    describe("수익금과 수익률") {
+        it ("5등 수익금") {
+            val matchingResult = MatchingResult()
+            matchingResult.match(Rank.FIFTH)
+
+            assertThat(matchingResult.totalWinningMoney()).isEqualTo(5_000)
+        }
+
+        it ("수익율") {
+            val matchingResult = MatchingResult()
+            matchingResult.match(Rank.FIFTH)
+
+            assertThat(matchingResult.getProfit(20_000)).isEqualTo(-75.0)
+        }
+    }
 })
